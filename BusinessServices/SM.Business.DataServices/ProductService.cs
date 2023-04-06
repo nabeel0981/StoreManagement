1514052f7ee9870359ccf8d5701cc48b5ed146c8
nabeel0981 <Nabeel.Leghari@itgs.pk>
2023-04-06 22:33:55 +0500

    Crud Operations Performed

---
+++ BusinessServices/SM.Business.DataServices/ProductService.cs
@@ -0,0 +1,37 @@
+﻿using MS.Business.Models;
+
+namespace SM.Business.DataServices
+{
+    public class ProductService
+    {
+        private List<ProductModel> product = new List<ProductModel>();
+
+        public List<ProductModel> GetAllProducts() 
+        {
+            product.Add(new ProductModel { Id = 1, Name = "Product1" });
+            product.Add(new ProductModel { Id = 2, Name = "Product2" });
+            product.Add(new ProductModel { Id = 3, Name = "Product3" });
+            product.Add(new ProductModel { Id = 4, Name = "Product4" });
+            product.Add(new ProductModel { Id = 5, Name = "Product5" });
+            return product;
+        }
+
+        public void Add(ProductModel model)
+        {
+            
+            product.Add(model);
+          
+        }
+      
+       
+        public void DeleteProduct(int id)
+        {
+            var prod = product.Where(x => x.Id == id).FirstOrDefault();
+            if (prod != null)
+            {
+                product.Remove(prod);
+                
+            }
+     }
+    }
+}
