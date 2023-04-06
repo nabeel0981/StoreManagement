9f98a5dd0826349917ac968b4b0b53c385a2f438
nabeel0981 <Nabeel.Leghari@itgs.pk>
2023-04-06 20:25:25 +0500

    Added Product Model

---
+++ Presentation/StoreManagement/Controllers/ProductController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MS.Business.Models;
+
+namespace StoreManagement.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: ProductController
+        public ActionResult Index()
+        {
+            var product = new List<ProductModel>();
+            product.Add(new ProductModel { Id=1 , Name ="Product1"});
+            product.Add(new ProductModel { Id = 2, Name = "Product2" });
+            product.Add(new ProductModel { Id = 3, Name = "Product3" });
+            product.Add(new ProductModel { Id = 4, Name = "Product4" });
+            product.Add(new ProductModel { Id = 5, Name = "Product5" });
+            return View(product);
+        }
+
+        // GET: ProductController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ProductController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProductController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ProductController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ProductController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
